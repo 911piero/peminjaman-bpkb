1f0c192a3c247e1316e04287d595b53a782a8aaf
khrisna-yudhap <86khrisnayudha@gmail.com>
2022-09-22 15:06:13 +0700

    kop

--- app/Views/peminjam_bpkb/print_pengantar.php
+++ app/Views/peminjam_bpkb/print_pengantar.php
@@ -7,20 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+        .kop {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <div class="col" style="border: 1px solid red;">
-            <div class="float-left" style="border:1px solid black;">
-                <img class="" src="<?= base_url('adminlte/dist/img/Logo.png'); ?>" height="150" width="100">
+        <div class="kop">
+            <div class="logo">
+                <img src="<?= base_url('adminlte/dist/img/Logo.png'); ?>" height="150" width="100">
             </div>
-            <div class="text-center" style="border: 1px solid black ;">
+            <div class="logo-text text-center" style="border: 1px solid black ;">
                 <h3>Pemerintah Kota</h3>
                 <h2><b>BADAN PENGELOLAAN KEUANGAN <br>DAN ASET DAERAH</b></h2>
                 <img src="<?= base_url('adminlte/dist/img/hanacaraka.png'); ?>">
             </div>
-
         </div>
     </div>
 </body>
